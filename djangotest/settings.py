d7ccbaccf39b9e760369eb34b543ba9fc5378252
Christopher Flores <kershingf@gmail.com>
2021-02-07 14:36:06 -0600

    add static and media to settings

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import socket
 
 SECRET_KEY = '@yce&^=ue^lqa_w)wa1mc$d_y8qm74gdqwh+es_(^awggc0$+b'
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_media")
